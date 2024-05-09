fb54772198bc0084c3b593abb26bb647bcb26a19
Prvzalm <prvzalm69@gmail.com>
2024-05-09 18:47:40 +0530

    Navbar fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { logo } from '../../assets';
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="flex justify-between items-center text-white p-4 fixed top-8 left-0 w-full z-50 px-[7.5rem]">
+    <nav className={`flex justify-between items-center text-white p-4 fixed transition-all duration-300 ${scrolled ? 'top-0' : 'top-8'} left-0 w-full z-50 px-[7.5rem] ${scrolled ? 'bg-transparent' : ''}`}>
       <div className="font-bold">
         <img className='w-36' src={logo} alt="" />
       </div>
-      <ul className="flex space-x-24 px-20 py-4 rounded-full bg-[#c1c1c1] bg-opacity-10">
+      <ul className={`flex space-x-24 px-20 py-4 rounded-full ${scrolled ? 'bg-[black] bg-opacity-60' : 'bg-[#c1c1c1] bg-opacity-10'}`}>
         <li>Home</li>
         <li>About</li>
         <li>Feature</li>
